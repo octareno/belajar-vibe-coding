dcf2e863fdaf090668a1da4520e9603278ac479f
Octareno <octareno.heddy@gmail.com>
2026-04-22 23:10:36 +0700

    feat: initialize backend project with Bun, Elysia, and Drizzle

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Elysia } from "elysia";
+
+const app = new Elysia()
+  .get("/", () => "Hello dari Elysia & Bun!")
+  .listen(3000);
+
+console.log(
+  `🚀 Server is running at ${app.server?.hostname}:${app.server?.port}`
+);
---
+++ src/schema.ts
@@ -0,0 +1,7 @@
+import { mysqlTable, serial, text } from "drizzle-orm/mysql-core";
+
+export const users = mysqlTable("users", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull(),
+});
